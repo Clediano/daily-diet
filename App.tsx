99df334cfe041732d00e76947b10c861cc637956
Clediano <cledianoestefenon@gmail.com>
2022-12-05 21:40:23 -0300

    configures font and typescript configurations

--- App.tsx
+++ App.tsx
@@ -1,7 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts, NunitoSans_400Regular, NunitoSans_700Bold } from '@expo-google-fonts/nunito-sans';
+
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    NunitoSans_400Regular,
+    NunitoSans_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+  
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
